1184ebb038476fc3639307f3779414e0e151167e
Keenan Graham <ksgraham@stanford.edu>
2023-01-09 20:05:39 -0800

    Add buckets with CORS

--- app.py
+++ app.py
@@ -1,23 +1,91 @@
 from aws_cdk import App
 from aws_cdk import Stack
+from aws_cdk import RemovalPolicy
+
+from aws_cdk.aws_s3 import Bucket
+from aws_cdk.aws_s3 import CorsRule
+from aws_cdk.aws_s3 import HttpMethods
 
 from constructs import Construct
 
 from shared_infrastructure.cherry_lab.environments import US_WEST_2
 
 
-class BucketWithPolicy(Stack):
+CORS = CorsRule(
+    allowed_methods=[
+        HttpMethods.GET,
+        HttpMethods.HEAD,
+    ],
+    allowed_origins=[
+        '*'
+    ],
+    allowed_headers=[
+        'Accept',
+        'Origin',
+        'Range',
+        'X-Requested-With',
+        'Cache-Control',
+    ],
+    exposed_headers=[
+        'Content-Length',
+        'Content-Range',
+        'Content-Type',
+    ],
+    max_age=3000,
+)
+
+
+class BucketStorage(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        self.blobs_logs_bucket = Bucket(
+            self,
+            'BlobsLogsBucket',
+            bucket_name='test-igvf-blobs-logs-dev',
+            removal_policy=RemovalPolicy.RETAIN,
+        )
+
+        self.blobs_bucket = Bucket(
+            self,
+            'BlobsBucket',
+            bucket_name='test-igvf-blobs-dev',
+            cors=[
+                CORS
+            ],
+            removal_policy=RemovalPolicy.RETAIN,
+            server_access_logs_bucket=self.blobs_logs_bucket,
+            versioned=True,
+        )
+
+        self.files_logs_bucket = Bucket(
+            self,
+            'FilesLogsBucket',
+            bucket_name='test-igvf-files-logs-dev',
+            removal_policy=RemovalPolicy.RETAIN,
+        )
+
+        self.files_bucket = Bucket(
+            self,
+            'FilesBucket',
+            bucket_name='test-igvf-files-dev',
+            cors=[
+                CORS
+            ],
+            removal_policy=RemovalPolicy.RETAIN,
+            server_access_logs_bucket=self.files_logs_bucket,
+            versioned=True,
+        )
 
 
 app = App()
 
 
-bucket_with_policy = BucketWithPolicy(
+bucket_storage = BucketStorage(
     app,
-    'BucketWithPolicyStack',
+    'BucketStorage',
     env=US_WEST_2,
+    termination_protection=True,
 )
 
 
